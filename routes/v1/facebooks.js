7dd4b12c805299485e812c0c4c9963b457261c3c
Stamp <stamp@sinoze.com>
2016-11-30 13:52:24 +0700

    add facebook authen to get playerID

---
+++ routes/v1/facebooks.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const constant = require(path.join(__base, 'helpers', 'constant'))
+
+const User = require(path.join(__base, 'models', 'User'))
+
+module.exports = {
+  // get a user's data
+  get (req, res, next) {
+    const userId = req.params.userId
+    new User()
+      .findBySomeId('facebook', userId)
+      .then(result => ({ data: { playerID: result.data.playerID }}))
+      .then(result => res.send(result))
+      .catch(next)
+  }
+}
